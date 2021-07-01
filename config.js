53d43d4bd43b584a40c340a86997e2da65214fd1
phuctan01 <86761355+phuctan01@users.noreply.github.com>
2021-07-01 11:58:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé Bae.',
     btnAccept: 'Dạaaaa <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/phuctan01' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
